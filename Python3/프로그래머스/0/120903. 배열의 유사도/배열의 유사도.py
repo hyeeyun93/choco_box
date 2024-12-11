dce34ff82c11a65f3b371ba900af4737de94f42f
hyeeyun93 <tkdcjxntjddl93@gmail.com>
2024-12-11 09:50:03 +0900

    [level 0] Title: 배열의 유사도, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- Python3/프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
+++ Python3/프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
@@ -1,8 +1,3 @@
 def solution(s1, s2):
-    answer = 0
-    for t in s1:
-        if t in s2:
-            answer += 1
-        else:
-            answer
+    answer = len([t for t in s1 if t in s2])
     return answer
