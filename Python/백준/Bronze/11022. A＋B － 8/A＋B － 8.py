5ae734bb847490f0a680d8eff9913ca4f983f2a8
Helen Sim <tkdcjxntjddl93@gmail.com>
2024-12-24 15:11:17 +0900

    [Bronze V] Title: A+B - 8, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -0,0 +1,12 @@
+import sys
+
+test_case = int(sys.stdin.readline().rstrip())
+
+test_case_list = []
+
+for _ in range(test_case):
+    A, B = map(int, sys.stdin.readline().rstrip().split())
+    test_case_list.append((A, B))
+
+for n in range(len(test_case_list)):
+    print(f'Case #{n + 1}: {test_case_list[n][0]} + {test_case_list[n][1]} = {sum(test_case_list[n])}')
