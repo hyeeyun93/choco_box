bea290a21cef706354ca78f6629d8703b1a14ea6
Helen Sim <tkdcjxntjddl93@gmail.com>
2025-01-02 11:35:02 +0900

    [level 0] Title: 합성수 찾기, Time: 0.11 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
+++ Python3/프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -1,14 +1,9 @@
 def solution(n):
     answer = 0
-    for i in range(1, n+1):
+    for i in range(4, n+1):
         count = 0
-        for j in range(1, int(i**0.5)+1):
+        for j in range(2, int(i**0.5)+1):
             if i % j == 0:
-                count += 1
-                if i != j:
-                    count += 1
-            if count >= 3:
+                answer += 1
                 break
-        if count >= 3:
-            answer += 1
     return answer
