b4e5c549697191f0a374dbbe9d143a37ecc8d9d1
Helen Sim <tkdcjxntjddl93@gmail.com>
2025-01-07 09:15:34 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ MySQL/프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -0,0 +1,11 @@
+select sum(hg.score) as score,
+       he.emp_no,
+       he.emp_name,
+       he.position,
+       he.email
+from hr_employees as he
+join hr_grade as hg
+on he.emp_no = hg.emp_no
+group by he.emp_no
+order by score desc
+limit 1 ;
