ed72199debf50e0056e5d45ad48acf4fbced4cf0
Helen Sim <tkdcjxntjddl93@gmail.com>
2025-01-21 15:01:26 +0900

    [Silver V] Title: 너의 평점은, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,18 @@
+grade_score = {'A+':4.5,
+               'A0':4.0,
+               'B+':3.5,
+               'B0':3.0,
+               'C+':2.5,
+               'C0':2.0,
+               'D+':1.5,
+               'D0':1.0,
+               'F':0.0}
+scores = []
+not_p_score = []
+for _ in range(20):
+    subject, score, grade = input().split()
+    if grade != 'P':
+        scores.append(float(score) * grade_score[grade])
+        not_p_score.append(float(score))
+
+print(sum(scores)/sum(not_p_score))
