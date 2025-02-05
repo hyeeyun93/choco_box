79d0f0e1997f7b8ece9fa2d2ba1bca17d06bbcbc
Helen Sim <tkdcjxntjddl93@gmail.com>
2025-02-05 10:51:35 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,11 +1,7 @@
 import math
 
 def solution(numer1, denom1, numer2, denom2):
-    answer = []
-    if denom1 == denom2:
-        answer.extend([numer1+numer2, denom1])
-    else:
-        answer.extend([(numer1*denom2)+(numer2*denom1), denom1*denom2])
+    answer = [(numer1*denom2)+(numer2*denom1), denom1*denom2]
     
     answer_gcd = math.gcd(answer[0], answer[1])
     
