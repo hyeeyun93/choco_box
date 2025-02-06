44faab2a189adaf5c7b50042aedb495e6c7e2009
Helen Sim <tkdcjxntjddl93@gmail.com>
2025-02-06 13:20:19 +0900

    [level 0] Title: 안전지대, Time: 0.10 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/120866. 안전지대/안전지대.py
@@ -0,0 +1,13 @@
+def solution(board):
+    size = len(board[0])
+    answer = size * size
+    bomb = set()
+    
+    for x in range(size):
+        for y in range(size):
+            if board[x][y] == 1:
+                for i in range(max(0, x-1), min(size, x+2)):
+                    for j in range(max(0, y-1), min(size, y+2)):
+                        bomb.add((i, j))
+                        
+    return answer - len(bomb)
