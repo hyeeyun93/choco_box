aba133c04b142346f5f93e498ed774d943fda716
Helen Sim <tkdcjxntjddl93@gmail.com>
2025-02-19 13:35:12 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.py
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.py
@@ -1,9 +1,5 @@
 def solution(arr, idx):
-    answer = 0
     for i in range(idx, len(arr)):
         if arr[i] == 1:
-            answer = i
-            break
-        elif 1 not in arr[idx:]:
-            answer = -1
-    return answer
+            return i
+    return -1
