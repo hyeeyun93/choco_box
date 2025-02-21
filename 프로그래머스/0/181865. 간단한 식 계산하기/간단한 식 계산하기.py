ee8ce2a58b22bde3cd1b5e5c2e5622ea3acb391c
Helen Sim <tkdcjxntjddl93@gmail.com>
2025-02-21 20:37:24 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.py
@@ -0,0 +1,9 @@
+def solution(binomial):
+    a, op, b = binomial.split()
+    if op == '+':
+        answer = int(a) + int(b)
+    elif op == '-':
+        answer = int(a) - int(b)
+    else:
+        answer = int(a) * int(b)
+    return answer
